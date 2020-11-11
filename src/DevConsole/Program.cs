42c126b857a70012f8c10676021c783337ef34df
Richard Niemand <niemand.richard@gmail.com>
2020-11-11 15:14:26 -0700

    Setting up start of DI

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GrafanaCli.DevConsole
 {
   class Program
   {
+    private static IServiceProvider _provider;
+
     static void Main(string[] args)
     {
+      SetupDIContainer();
+
       Console.WriteLine("Hello World!");
+    }
+
+    private static void SetupDIContainer()
+    {
+      var collection = new ServiceCollection();
+
+      var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .Build();
+
+      _provider = collection.BuildServiceProvider();
     }
   }
 }
